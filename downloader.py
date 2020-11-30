f66337332e5f127affcd70156954250623b3dcba
huhailang <huhailang@comtom.cn>
2020-11-30 22:04:04 +0800

    1.视频下载器、视频切分器技术预研

--- downloader.py
+++ downloader.py
@@ -1,19 +1,17 @@
 import os
 from datetime import datetime
-
+from ffmpy import FFmpeg
 dest = 'E:/TV/'
 
 def download(url: str) :
-    """:param video
-       ffmpeg -i 3.mp4 -vn -y -acodec copy 3.aac
-       ffmpeg -i 2018.mp4 -codec copy -bsf: h264_mp4toannexb -f h264 tmp.264
-       ffmpeg -i killer.mp4 -an -vcodec copy out.h264
-    """
-    # ffmpeg_cmd = 'ffmpeg -i {} -vn -y -acodec copy {}'
-    ffmpeg_cmd = 'ffmpeg -i {} -t 15 -c copy {}'
-    now = datetime.now().strftime('%Y%m%d%H%M%S')
-    cmd = ffmpeg_cmd.format(url, dest + now + '.flv')
-    os.system(cmd)
+    name = datetime.now().strftime('%Y%m%d%H%M%S%f')
+    ff = FFmpeg(
+         inputs={url: None},
+         outputs={
+              'E:/TV/' + name + '.flv': '-loglevel quiet -t 10 -vcodec h264 -acodec aac -c:a copy'}
+    )
+    print(ff.cmd)
+    ff.run()
 
 
 if __name__ == '__main__':
--- generator.py
+++ generator.py
@@ -1,32 +1,28 @@
-# coding=gbk
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import os
 from ffmpy import FFmpeg
 import threading
 import time
 from datetime import datetime
-dest = 'E:/TV/'
-url = 'http://tx2play1.douyucdn.cn/live/2045445r9wqXizbz.flv?uuid='
+from concurrent.futures import ThreadPoolExecutor
 
 
 def download(url: str) :
     print('current thread', threading.current_thread().name)
-    ffmpeg_cmd = 'ffmpeg -loglevel quiet -i  {} -t 5 -s 1280x720 -c:a copy {}'
-    now = datetime.now().strftime('%Y%m%d%H%M%S')
-    cmd = ffmpeg_cmd.format(url, dest + now + '.flv')
-    os.system(cmd)
-    return dest + now + '.flv'
-
-
-def info():
-    ffmpeg_cmd = 'ffmpeg -i http://tx2play1.douyucdn.cn/live/2045445r9wqXizbz.flv?uuid='
-    print(ffmpeg_cmd)
-    os.system(ffmpeg_cmd)
-
-if __name__ == '__main__':
-    # executor = ThreadPoolExecutor(max_workers=1)
-    # while True:
-    #     executor.submit(download, (url))
-    #     time.sleep(2)
-    info()
-
+    name = datetime.now().strftime('%Y%m%d%H%M%S%f')
+    ff = FFmpeg(
+         inputs={url: None},
+         outputs={
+              'E:/TV/' + name + '.flv': '-loglevel quiet -t 10 -vcodec h264 -acodec aac -c:a copy'}
+    )
+    print(ff.cmd)
+    ff.run()
+#
+#
+executor = ThreadPoolExecutor(max_workers=2)
+n = 0
+while True:
+    n = n + 1
+    executor.submit(download, ('http://tx2play1.douyucdn.cn/live/6448043r1vyShOLw.flv?uuid='))
+    if n == 1:
+        time.sleep(10)
+    else:
+        time.sleep(1)
